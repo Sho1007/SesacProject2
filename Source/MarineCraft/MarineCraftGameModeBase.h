bcef98ea75a1d8af3be9a5ddd4b8b6c74cd3757a
Sho1007 <sho1007@kookmin.ac.kr>
2023-11-07 15:06:35 +0900

    Create Unreal Project / Add gitignore file

---
+++ Source/MarineCraft/MarineCraftGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MarineCraftGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MARINECRAFT_API AMarineCraftGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
